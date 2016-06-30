09374513f8d9e1ed1f2eca35118e6da0043de6ce
Plamen Yankov <p.s.yankov@abv.bg>
2016-06-30 08:19:45 +0300

    Initial support for linting

---
+++ jakefile.js
@@ -0,0 +1,54 @@
+/* globals directory:false, desc:false, task:false, fail:false, complete:false, jake:false  */
+
+(function(){
+   "use strict";
+   var jshint = require('simplebuild-jshint');
+
+   task("default",["lint"],function(){
+      console.log("jake Hello");
+   });
+
+   desc("Linting");
+   task("lint", function () {
+      process.stdout.write("Javascript linting ");
+      jshint.checkFiles({
+         files: ["jakefile.js"],
+         options: lintOpt(),
+         globals: lintGlobals()
+      }, complete, fail);
+   }, {async: true});
+
+
+   // Linting Options Functions
+
+   function lintOpt() {
+      return {
+         bitwise: true,
+         eqeqeq: true,
+         forin: true,
+         freeze: true,
+         futurehostile: true,
+         latedef: "nofunc",
+         noarg: true,
+         nocomma: true,
+         nonbsp: true,
+         nonew: true,
+         strict: true,
+         undef: true,
+         node: true,
+         browser: true
+      };
+   }
+
+   function lintGlobals() {
+      return {
+         //Mocha
+         describe: false,
+         it: false,
+         before: false,
+         after: false,
+         beforeEach: false,
+         afterEach: false
+      };
+   }
+}());
